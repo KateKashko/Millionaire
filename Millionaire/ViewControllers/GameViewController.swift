cfb9d992a97e3dfd018067484721e2cde2fcb3e2
KateKashko <107617602+KateKashko@users.noreply.github.com>
2024-02-25 22:10:11 +0300

    create ViewControllers

---
+++ Millionaire/ViewControllers/GameViewController.swift
@@ -0,0 +1,29 @@
+//
+//  GameViewController.swift
+//  Millionaire
+//
+//  Created by Kate Kashko on 25.02.2024.
+//
+
+import UIKit
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
