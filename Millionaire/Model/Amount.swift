ee31e445bc2fbdf1f4363ed0b9fcbd81eecf5ae3
Pavel Shirokiy <pshirokiy@gmail.com>
2024-02-26 22:41:17 +0300

    do the Amount models

---
+++ Millionaire/Model/Amount.swift
@@ -0,0 +1,15 @@
+//
+//  AmoutArray.swift
+//  Millionaire
+//
+//  Created by Павел Широкий on 26.02.2024.
+//
+
+import Foundation
+
+struct AmoutArray {
+    
+    let amount: Int
+    let sumOfPrice: Int
+    
+}
---
+++ Millionaire/Model/SumOfPrice.swift
@@ -0,0 +1,8 @@
+//
+//  SumOfPrice.swift
+//  Millionaire
+//
+//  Created by Павел Широкий on 26.02.2024.
+//
+
+import Foundation
