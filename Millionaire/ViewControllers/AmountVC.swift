992b27b09039827b64711c0689676771be8cde17
Pavel Shirokiy <pshirokiy@gmail.com>
2024-02-26 22:02:40 +0300

    first, test

--- Millionaire/ViewControllers/AmountVC.swift
+++ Millionaire/ViewControllers/AmountVC.swift
@@ -9,14 +9,13 @@ import UIKit
 
 class AmountVC: UITableViewController {
 
+    let backGround = UIImageView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        
+        setupBackground()
+        
     }
 
     // MARK: - Table view data source
@@ -31,6 +30,22 @@ class AmountVC: UITableViewController {
         return 0
     }
 
+    private func setupBackground() {
+        view.addSubview(backGround)
+        backGround.image = UIImage(named: "mainBG")
+        backGround.contentMode = .scaleAspectFill
+        
+        backGround.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            backGround.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            backGround.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            backGround.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            backGround.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+        ])
+        
+    }
+    
+    
     /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
